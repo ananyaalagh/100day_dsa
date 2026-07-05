42c19ff1a6e1147665859420e8ded2593093d971
ananya <ananyaalagh10@gmail.com>
2026-07-05 18:54:11 +0530

    Day 9 solutions

---
+++ Day9.java
@@ -0,0 +1,33 @@
+// Q1
+import java.util.Scanner;
+
+public class Day9{
+          public static void main(String[] args){
+                 
+          Scanner sc= new Scanner(System.in);
+          System.out.println("Enter the forward code:");
+          String str=sc.nextLine();
+          int n= str.length();
+          for(int i=n-1;i>=0; i--){
+                    char ch= str.charAt(i);
+                    System.out.print(ch);
+          }
+          }
+
+}
+
+// Q2
+class Solution {
+    public void reverseString(char[] s) {
+        int n=s.length;
+        int left=0;
+        int right=n-1;
+        while(left<right){
+            char temp=s[left];
+            s[left]=s[right];
+            s[right]=temp;
+            left++;
+            right--;
+        }
+    }
+}
